359bc5ca919038a5b37f390c08e40ae09784f379
lAcewingl <acewing@love.com>
2019-12-20 11:07:25 -0600

    My site
    First launch

---
+++ js/site.js
@@ -0,0 +1,12 @@
+//jQuery(function($) {
+  //  alert("let's gooo!");
+// });
+
+
+var animation = lottie.loadAnimation({
+    container: document.getElementById('bm'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'data.json'
+})
